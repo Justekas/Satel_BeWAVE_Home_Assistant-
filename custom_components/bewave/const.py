d89eb79ea06bce4971ad04f54a99d0abdf4bf968
Slave IT Solutions <kalo-pc@windowslive.com>
2026-06-09 21:57:00 +0300

    Add BE WAVE integration files

---
+++ custom_components/bewave/const.py
@@ -0,0 +1,12 @@
+DOMAIN = "bewave"
+
+CONF_LOGIN = "login"
+CONF_PASSWORD = "password"
+CONF_HOST = "host"
+CONF_SERIAL = "serial"
+CONF_DEVICE_UUID = "device_uuid"
+CONF_MODE = "mode"
+
+DEFAULT_MODE = "defau"   # "default" protection mode key
+DEFAULT_PORT = 4200
+SCAN_INTERVAL_SECONDS = 5
